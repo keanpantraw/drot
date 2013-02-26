0070a40898d4c11df53510854927312e3b85738f
Boris Filippov <bfilippov@griddynamics.com>
2013-02-27 03:42:10 +0400

    del drot["Object Oriented Programming"]

--- drot/api.py
+++ drot/api.py
@@ -1,4 +1,81 @@
+import functools
 import inspect
+
+
+FORMATTER = 'formatter'
+PARSER = 'parser'
+FORMATTERS = '__drot_formatters'
+PARSERS = '__drot_parsers'
+TYPE = '__drot_type'
+ATTRIBUTE = '__drot_attribute'
+
+
+def definition(clazz):
+    """Decorate class, making it suitable for dict <-> object conversion"""
+    @classmethod
+    def to_object(cls, *args, **kwargs):
+        for name, parser in getattr(cls, PARSERS, {}):
+            if name in kwargs:
+                kwargs[name] = parser(kwargs[name])
+        return clazz(*args, **kwargs)
+
+    clazz.to_dict = _to_dict
+    clazz.to_object = to_object
+    _collect_schema_info(clazz)
+
+    clazz.__init__ = _decorate_init(clazz.__init__)
+
+
+def formatter(field_name):
+    """Formatter that will be used to format field_name
+    during transformation to dictionary.
+    Returned value will be used literally in output dictionary.
+    """
+    return _patch_method(field_name, FORMATTER)
+
+
+def parser(field_name):
+    """Parser that will be used to parse dictionary field
+    with name field_name before creation of corresponding object
+    """
+    return _patch_method(field_name, PARSER)
+
+
+def _decorate_init(initializer):
+    """Detect class attributes for serialization"""
+    @functools.wraps(initializer)
+    def wrapper(self, *args, **kwargs):
+        self._mapping_attributes = kwargs.keys()
+        initializer(self, *args, **kwargs)
+    return wrapper
+
+
+def _collect_schema_info(cls):
+    """Collect formatters and parsers"""
+    _collect_methods(cls, FORMATTERS,
+                     FORMATTER)
+    _collect_methods(cls, PARSERS,
+                     PARSER)
+
+
+def _collect_methods(cls, storage_name, method_type):
+    if not hasattr(cls, storage_name):
+        collected = dict((getattr(x, ATTRIBUTE), x)
+                         for x in inspect.getmembers(cls, inspect.ismethod)
+                         if _drot_type(x) == method_type)
+        setattr(cls, storage_name, collected)
+
+
+def _drot_type(method):
+    return getattr(method, TYPE, None)
+
+
+def _patch_method(field_name, drot_type):
+    def wrapper(method):
+        setattr(method, TYPE, drot_type)
+        setattr(method, ATTRIBUTE, field_name)
+        return method
+    return wrapper
 
 
 def _transform_item(item):
@@ -32,72 +109,3 @@ def _to_dict(self):
             transform = self.__drot_formatters.get(key, _transform_item)
             result[key] = transform(item)
     return result
-
-
-class Definition(object):
-    FORMATTER = 'formatter'
-    PARSER = 'parser'
-    FORMATTERS = '__drot_formatters'
-    PARSERS = '__drot_parsers'
-    TYPE = '__drot_type'
-    ATTRIBUTE = '__drot_attribute'
-
-    def __init__(self, clazz):
-        self.cls = clazz
-
-        @classmethod
-        def to_object(cls, *args, **kwargs):
-            for name, parser in getattr(cls, Definition.PARSERS, {}):
-                if name in kwargs:
-                    kwargs[name] = parser(kwargs[name])
-            return self.cls(*args, **kwargs)
-
-        self.cls.to_dict = _to_dict
-        self.cls.to_object = to_object
-        self._collect_schema_info()
-
-    def __call__(self, *args, **kwargs):
-        """Detect class attributes for serialization"""
-        instance = self.cls(*args, **kwargs)
-        instance._mapping_attributes = kwargs.keys()
-        return instance
-
-    def _collect_schema_info(self):
-        """Collect formatters and parsers"""
-        self._collect_methods(self.cls, Definition.FORMATTERS,
-                              Definition.FORMATTER)
-        self._collect_methods(self.cls, Definition.PARSERS,
-                              Definition.PARSER)
-
-    def _collect_methods(self, cls, storage_name, method_type):
-        if not hasattr(cls, storage_name):
-            collected = dict((getattr(x, Definition.ATTRIBUTE), x)
-                             for x in inspect.getmembers(cls, inspect.ismethod)
-                             if self._drot_type(x) == method_type)
-            setattr(cls, storage_name, collected)
-
-    def _drot_type(self, method):
-        return getattr(method, Definition.TYPE, None)
-
-    @classmethod
-    def formatter(cls, field_name):
-        """Formatter that will be used to format field_name
-        during transformation to dictionary.
-        Returned value will be used literally in output dictionary.
-        """
-        return cls._patch_method(field_name, Definition.FORMATTER)
-
-    @classmethod
-    def parser(cls, field_name):
-        """Parser that will be used to parse dictionary field
-        with name field_name before creation of corresponding object
-        """
-        return cls._patch_method(field_name, Definition.PARSER)
-
-    @classmethod
-    def _patch_method(cls, field_name, drot_type):
-        def wrapper(method):
-            setattr(method, Definition.TYPE, drot_type)
-            setattr(method, Definition.ATTRIBUTE, field_name)
-            return method
-        return wrapper
